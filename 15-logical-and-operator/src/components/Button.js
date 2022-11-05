753ece4712bce06cc098078cba7804a85d94be71
WildMrDeFold <wildmrdefox@gmail.com>
2022-11-05 06:10:56 +0300

    Использование оператора И

---
+++ 15-logical-and-operator/src/components/Button.js
@@ -0,0 +1,7 @@
+function Button({onClick}) {
+  return(
+    <button onClick={onClick}>Click Me!</button>
+  )
+}
+
+export default Button;
---
+++ 15-logical-and-operator/src/components/ResetButton.js
@@ -0,0 +1,16 @@
+function ResetButton({count ,onClick}) {
+  const buttonStyle = { backgroundColor: "lightgreen" };
+  return (
+    <div>
+      {count > 0 && (
+        <div>
+          <button style={buttonStyle} onClick={onClick}>
+            Reset
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ResetButton;
