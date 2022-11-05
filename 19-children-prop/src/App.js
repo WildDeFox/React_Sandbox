90a9e3264f02aab08c078b15759cb4c67e50395a
WildMrDeFold <wildmrdefox@gmail.com>
2022-11-05 14:55:30 +0300

    использование свойства children

---
+++ 19-children-prop/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Wrapper from './components/Wrapper';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Wrapper color="red">
+        <h2>Text inside from the Wrapper</h2>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
