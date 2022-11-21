e62b1354349969ff1a2d292cadf18544f036ba1d
WildMrDeFold <wildmrdefox@gmail.com>
2022-11-21 08:24:03 +0300

    Отключение кнопки удаления завершенных задач

--- 25-todo-app-v2/src/components/Todos/TodosActions.js
+++ 25-todo-app-v2/src/components/Todos/TodosActions.js
@@ -1,12 +1,13 @@
 import { RiDeleteBin2Line, RiRefreshLine } from "react-icons/ri";
 import Button from "../UI/Button";
+import styles from './TodosActions.module.css'
 
-function TodosActions({resetTodos, deleteCompletedTodos}) {
+function TodosActions({resetTodos, deleteCompletedTodos, completedTodosExist}) {
   return (
-    <>
+    <div className={styles.todosActionsContainer}>
     <Button title="Reset Todos" onClick={resetTodos}><RiRefreshLine /></Button>
-    <Button title="Clear Completed Todos" onClick={deleteCompletedTodos}><RiDeleteBin2Line /></Button>
-    </>
+    <Button title="Clear Completed Todos" onClick={deleteCompletedTodos} disabled={!completedTodosExist}><RiDeleteBin2Line /></Button>
+    </div>
   );
 }
 
